13149dc8fc2a8d945c3113937c6842114e56a72e
Pedro Sanchez <p.sanchez@europe.com>
2020-06-06 13:15:26 +0200

    Layout for the editor, pending to add code editor

--- poe-fe/poe-fe/src/app/app-routing.module.ts
+++ poe-fe/poe-fe/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EditorComponent } from './components/editor/editor.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'editor', component: EditorComponent },
+  { path: '', redirectTo: '/editor', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- poe-fe/poe-fe/src/app/app.module.ts
+++ poe-fe/poe-fe/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EditorComponent } from './components/editor/editor.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
