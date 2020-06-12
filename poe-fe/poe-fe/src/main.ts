342e7f186565668dc3a2a59a593e4e033b2169e9
Pedro Sanchez <p.sanchez@europe.com>
2020-06-12 19:21:23 +0200

    Added custom highlighting for PDDL systax

--- poe-fe/poe-fe/src/main.ts
+++ poe-fe/poe-fe/src/main.ts
@@ -3,7 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import 'codemirror/mode/scheme/scheme';
+import './assets/codemirror/pddl-mode';
 
 if (environment.production) {
   enableProdMode();
