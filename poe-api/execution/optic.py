a9c8e7d59091bcdf6c6fabefe91fac4e49a57ccc
Pedro Sanchez <p.sanchez@europe.com>
2020-09-27 19:52:55 +0200

    Forgot to move the import of TEMP_FOLDER constant

--- poe-api/execution/optic.py
+++ poe-api/execution/optic.py
@@ -1,15 +1,15 @@
 import os
 
 from subprocess import STDOUT, PIPE, check_output, CalledProcessError
-from  execution import planner_exec
+from filesmgr import files_manager
 
 # CONSTANTS
 OPTIC_BIN = "./bin/optic-clp"
 
 # Execute optic
 def execute(file_names, timeout):
-    domain_file_name = os.path.join(planner_exec.TEMP_FOLDER, file_names[0])
-    problem_file_name = os.path.join(planner_exec.TEMP_FOLDER, file_names[1])
+    domain_file_name = os.path.join(files_manager.TEMP_FOLDER, file_names[0])
+    problem_file_name = os.path.join(files_manager.TEMP_FOLDER, file_names[1])
 
     try:
         output = check_output([ "timeout", "{}s".format(timeout), OPTIC_BIN, domain_file_name, problem_file_name], stderr=STDOUT, timeout=timeout+0.1)
--- poe-api/execution/sgplan.py
+++ poe-api/execution/sgplan.py
@@ -1,15 +1,15 @@
 import os
 
 from subprocess import STDOUT, PIPE, check_output, CalledProcessError
-from  execution import planner_exec
+from filesmgr import files_manager
 
 # CONSTANTS
 SGPLAN_BIN = "./bin/sgplan522"
 
 # Execute sgPlan
 def execute(file_names, timeout):
-    domain_file_name = os.path.join(planner_exec.TEMP_FOLDER, file_names[0])
-    problem_file_name = os.path.join(planner_exec.TEMP_FOLDER, file_names[1])
+    domain_file_name = os.path.join(files_manager.TEMP_FOLDER, file_names[0])
+    problem_file_name = os.path.join(files_manager.TEMP_FOLDER, file_names[1])
     
     try:
         output = check_output([SGPLAN_BIN, "-o", domain_file_name, "-f", problem_file_name],  stderr=STDOUT, timeout=timeout)
