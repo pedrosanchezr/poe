c269794d0ac0c3b45eacbdb4ba951b9d3698d649
Pedro Sanchez <p.sanchez@europe.com>
2020-10-04 21:24:17 +0200

    Latest changes for small screens. Should work well on desktops, laptop and tablets. Added some workarounds for smartphones but didn't check too much as this is not the priority (I don't think anyone will code on smartphones, just making it "work" would be enough).

--- poe-fe/poe-fe/src/app/components/editor/editor-box/editor-box.component.ts
+++ poe-fe/poe-fe/src/app/components/editor/editor-box/editor-box.component.ts
@@ -41,9 +41,6 @@ export class EditorBoxComponent implements OnInit {
   /** Event to switch between editors */
   @Output() switchEditor = new EventEmitter();
 
-  /** Workaround to fix the VH sizes on smartphones */
-  public vh = window.innerHeight * 0.01;
-
   /**
    * Editor font size (in pt)
    */
@@ -52,8 +49,6 @@ export class EditorBoxComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    // Workaround for the VH sizes on smartphones
-    document.documentElement.style.setProperty('--vh', `${this.vh}px`);
   }
 
   /**
